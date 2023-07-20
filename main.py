8fbee8850b1cc405dd7b41181b7a19bac82661be
Artur <arturkummer08@gmail.com>
2023-07-20 14:46:05 +0200

    0.99 beta

--- main.py
+++ main.py
@@ -1,106 +1,172 @@
+import os
 import random
 import time
 import requests
 import math
+import logging
+from tqdm import tqdm
+
 
 class ConnectionError(Exception):
     pass
 
+
 def connect_with_proxy(url, proxy):
-    proxies = {}
+    proxies = {"http": proxy, "https": proxy}
     try:
         response = requests.get(url, proxies=proxies, timeout=5)
-        print(f"Connected to the website using proxy: {proxy}")
-        print(f"Response status code: {response.status_code}")
+        logging.info(f"Connected to the website using proxy: {proxy}")
+        logging.info(f"Response status code: {response.status_code}")
+        return True
     except requests.exceptions.RequestException as e:
-        raise ConnectionError(f"\033[31mFailed to connect to the website using proxy: {proxy}: {str(e)}\033[0m")
+        raise ConnectionError(f"Failed to connect to the website using proxy: {proxy}: {str(e)}")
     except Exception as e:
-        raise ConnectionError(f"\033[31mAn error occurred: {str(e)}\033[0m")
+        raise ConnectionError(f"An error occurred: {str(e)}")
 
-def simulate_traffic(url, num_requests, choice, choice2, randomness):
+
+def simulate_traffic(url, num_requests, choice, choice2, randomness, retry_on_failure=True):
     times = 0
+    successful_requests = 0
 
     with open('list.txt') as f:
         proxies = f.read().splitlines()
 
-    for i in range(num_requests):
-        print(f"\033[32mConnected to website {times} times.\033[0m")
-        proxy = random.choice(proxies)
-        try:
-            connect_with_proxy(url, proxy)
-            times += 1
-        except ConnectionError as e:
-            print(str(e))
-            continue
+    for i in tqdm(range(num_requests), desc="Simulating Traffic", unit="request", bar_format="{l_bar}{bar}|"):
+        if retry_on_failure:
+            connected = False
+            while not connected:
+                logging.info(f"Connected to website {successful_requests} times.")
+                proxy = random.choice(proxies)
+                try:
+                    connect_with_proxy(url, proxy)
+                    times += 1
+                    successful_requests += 1
+                    connected = True
 
-        duration = randomisation(choice, randomness)
-        disconnect_duration = randomisation(choice2, randomness)
+                    duration = randomisation(choice, randomness)
+                    disconnect_duration = randomisation(choice2, randomness)
 
-        print(f"\033[33mStaying on the website for {duration} seconds...\033[0m")
-        time.sleep(duration)
+                    logging.info(f"Staying on the website for {duration:.2f} seconds...")
+                    time.sleep(duration)
 
-        print(f"\033[34mDisconnecting from the website for {disconnect_duration} seconds...\033[0m")
-        time.sleep(disconnect_duration)
+                    logging.info(f"Disconnecting from the website for {disconnect_duration:.2f} seconds...")
+                    time.sleep(disconnect_duration)
+
+                except ConnectionError as e:
+                    logging.error(str(e))
+                    if not retry_on_failure:
+                        break
+        else:
+            logging.info(f"Connected to website {successful_requests} times.")
+            proxy = random.choice(proxies)
+            try:
+                connect_with_proxy(url, proxy)
+                times += 1
+                successful_requests += 1
+
+                duration = randomisation(choice, randomness)
+                disconnect_duration = randomisation(choice2, randomness)
+
+                logging.info(f"Staying on the website for {duration:.2f} seconds...")
+                time.sleep(duration)
+
+                logging.info(f"Disconnecting from the website for {disconnect_duration:.2f} seconds...")
+                time.sleep(disconnect_duration)
+
+            except ConnectionError as e:
+                logging.warning(str(e))
+                if not retry_on_failure:
+                    break
+
 
 def randomisation(num, randomness):
     num += 1
-    random_shit = (math.pi*math.e*math.sqrt(2)*math.tau/10) * randomness/2
+    random_shit = (math.pi * math.e * math.sqrt(2) * math.tau / 10) * randomness / 2
     lowest_num = num - random_shit
     if lowest_num < 0:
         lowest_num = 0
     result = random.uniform(lowest_num, num + random_shit)
-    result = abs(result -1)
+    result = abs(result - 1)
     rounded_result = round(result, 2)
     return rounded_result
 
-valid_choices = ["slow", "medium", "fast", "heck", "manual"]
 
-while True:
-    choice = input("How fast do you want to connect to the website? (slow, medium, fast, or manual): ")
-    if choice == "manual":
+def get_user_choices():
+    valid_choices = ["slow", "medium", "fast", "heck", "manual"]
+    while True:
+        choice = input("How fast do you want to connect to the website? (slow, medium, fast, or manual): ")
+        if choice == "manual":
+            try:
+                print(
+                    "First choice is the average time of staying on the website, and choice2 is the average time of disconnecting from the website")
+                choice = int(input("Enter your choice (0-50): "))
+                if choice < 0 or choice > 50:
+                    raise ValueError("Choice must be between 0 and 50.")
+                choice2 = int(input("Enter your choice2 (0-50): "))
+                if choice2 < 0 or choice2 > 50:
+                    raise ValueError("Choice2 must be between 0 and 50.")
+                break
+            except ValueError as e:
+                print(f"Invalid input: {str(e)}")
+        else:
+            if choice in valid_choices:
+                if choice == "slow":
+                    choice, choice2 = 20, 10
+                elif choice == "medium":
+                    choice, choice2 = 10, 5
+                elif choice == "fast":
+                    choice, choice2 = 5, 2.5
+                elif choice == "heck":
+                    choice, choice2 = 0, 0
+                break
+            else:
+                print("Invalid choice.")
+    return choice, choice2
+
+
+def get_randomness():
+    while True:
         try:
-            print("First choice is the average time of staying on the website, and choice2 is the average time of disconnecting from the website")
-            choice = int(input("Enter your choice (0-50): "))
-            if choice < 0 or choice > 50:
-                raise ValueError("Choice must be between 0 and 50.")
-            choice2 = int(input("Enter your choice2 (0-50): "))
-            if choice2 < 0 or choice2 > 50:
-                raise ValueError("Choice2 must be between 0 and 50.")
-            break
-        except ValueError as e:
-            print(f"Invalid input: {str(e)}")
-    else:
-        if choice in valid_choices:
-            if choice == "slow":
-                choice, choice2 = 20, 10
-            elif choice == "medium":
-                choice, choice2 = 10, 5
-            elif choice == "fast":
-                choice, choice2 = 5, 2.5
-            elif choice == "heck":
-                choice, choice2 = 0, 0
-            break
-        else:
-            print("Invalid choice.")
+            randomness = float(input("How random would you like the connection time to be? (0 to 1): "))
+            if 0 <= randomness <= 1:
+                break
+            else:
+                print("Invalid input. Randomness must be between 0 and 1.")
+        except ValueError:
+            print("Invalid input. Please enter a valid float value between 0 and 1.")
+    return randomness
 
-while True:
+
+def setup_logging():
+    # Create the 'logs' folder if it doesn't exist
+    if not os.path.exists('logs'):
+        os.makedirs('logs')
+
+    # Get the current date to use in the log file name
+    current_date = time.strftime("%Y-%m-%d")
+    log_file = f"logs/traffic_simulation_{current_date}.log"
+
+    logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+if __name__ == "__main__":
+    setup_logging()
+
+    url = input("Enter the URL of the website to simulate traffic for: ")
+    while True:
+        try:
+            num_requests = int(input("How many requests do you want to make? "))
+            if num_requests > 0:
+                break
+            else:
+                print("Number of requests must be greater than 0.")
+        except ValueError:
+            print("Invalid input. Please enter a valid integer value.")
+
+    choice, choice2 = get_user_choices()
+    randomness = get_randomness()
+
     try:
-        randomness = float(input("How random would you like the connection time to be? (0 to 1): "))
-        if 0 <= randomness <= 1:
-            break
-        else:
-            print("Invalid input. Randomness must be between 0 and 1.")
-    except ValueError:
-        print("Invalid input. Please enter a valid float value between 0 and 1.")
-
-url = 'http://www.rutra.live'
-while True:
-    try:
-        num_requests = int(input("How many request do you want to make? "))
-        break
-    except ValueError:
-        print("Invalid input. ")
-
-
-
-simulate_traffic(url, num_requests, choice, choice2, randomness)
+        simulate_traffic(url, num_requests, choice, choice2, randomness, retry_on_failure=True)
+    except KeyboardInterrupt:
+        print("\nTraffic simulation terminated by the user.")
